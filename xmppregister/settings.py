a87187a24159eab3fd806f39994590f178660a39
Mathias Ertl <mati@fsinf.at>
2013-10-05 21:03:13 +0200

    import localsettings.py

--- xmppregister/settings.py
+++ xmppregister/settings.py
@@ -136,6 +136,11 @@ INSTALLED_APPS = (
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
+try:
+    from localsettings import *
+except ImportError:
+    pass
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
