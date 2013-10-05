9071fa9b52ee06feb0ca407a537409da3a91cbfc
Mathias Ertl <mati@fsinf.at>
2013-10-05 20:36:31 +0200

    rename backend class, fix licence text, add exception

--- backends/base.py
+++ backends/base.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of RestAuth (https://restauth.net).
+# This file is part of xmppregister (https://account.jabber.at/doc).
 #
-# RestAuth is free software: you can redistribute it and/or modify
+# xmppregister is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# RestAuth is distributed in the hope that it will be useful,
+# xmppregister is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with RestAuth.  If not, see <http://www.gnu.org/licenses/>.
+# along with xmppregister.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import unicode_literals
 
 
-class BackendBase(object):
+class InvalidXmppBackendError(Exception):
+    pass
+
+
+class XmppBackendBase(object):
     def create(self, username, host, password):
         raise NotImplementedError
 
