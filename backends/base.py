22b8a8505e00188e5906ceae7047f6e1d7dabf99
Mathias Ertl <mati@fsinf.at>
2013-10-05 20:21:20 +0200

    define backend interface

---
+++ backends/base.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of RestAuth (https://restauth.net).
+#
+# RestAuth is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RestAuth is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RestAuth.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+
+class BackendBase(object):
+    def create(self, username, host, password):
+        raise NotImplementedError
+
+    def check_password(self, username, host, password):
+        raise NotImplementedError
+
+    def set_password(self, username, host, password):
+        raise NotImplementedError
+
+    def remove(self, username, host):
+        raise NotImplementedError
+
+    def set_email(self, username, host):
+        raise NotImplementedError
