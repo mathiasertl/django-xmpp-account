7d470f8d4a40933f43c53a04f55fd95f186f93a1
Mathias Ertl <mati@fsinf.at>
2013-10-05 20:15:16 +0200

    add urls.py to register app

---
+++ register/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns
+from django.conf.urls import include
+from django.conf.urls import url
+
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'xmppregister.views.home', name='home'),
+    # url(r'^xmppregister/', include('xmppregister.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
