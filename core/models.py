b0ac823ff5f98869ce137f36356268e87f34e368
Mathias Ertl <mati@fsinf.at>
2013-10-13 23:52:37 +0200

    unfinished: use a custom user model

--- core/models.py
+++ core/models.py
@@ -20,16 +20,21 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.db import models
 
+from django.contrib.auth.models import AbstractBaseUser
 
-class Registration(models.Model):
-    username = models.CharField(max_length=1023)
+
+class Registration(AbstractBaseUser):
+    username = models.CharField(max_length=1023, unique=True)
     host = models.CharField(
         max_length=64,
         choices=tuple([(host, host) for host in settings.XMPP_HOSTS])
     )
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
 
     # when the account was first registered
     registered = models.DateTimeField(auto_now_add=True)
     # when the email was confirmed
     confirmed = models.DateTimeField(null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'host', 'email', 'registered', ]
