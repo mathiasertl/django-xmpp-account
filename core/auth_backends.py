35bff822d54b132e9ba6217074757726fac411fa
Mathias Ertl <mati@fsinf.at>
2013-10-20 00:23:29 +0200

    add authentication backend

---
+++ core/auth_backends.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of xmppregister (https://account.jabber.at/doc).
+#
+# xmppregister is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# xmppregister is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with xmppregister.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+from django.contrib.auth import get_user_model
+
+from backends import backend
+
+User = get_user_model()
+
+
+class BackendBackend(object):
+    def authenticate(self, username, password):
+        try:
+            user = User.objects.get(email=username)
+            if backend.check_password(user.username, user.domain, password):
+                return user
+        except User.DoesNotExist:
+            pass
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
