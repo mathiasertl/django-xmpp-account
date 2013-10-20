71c8bf035183556088046dda28bd2360fb578422
Mathias Ertl <mati@fsinf.at>
2013-10-20 20:28:19 +0200

    add fucntion to get random string

---
+++ core/utils.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of xmppregister (https://account.jabber.at/doc).
+#
+# xmppregister is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# xmppregister is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with xmppregister.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+import random
+import string
+
+SAFE_CHARS = string.ascii_letters + string.digits
+
+def random_string(length=32):
+    return ''.join(random.choice(SAFE_CHARS) for x in range(length))
