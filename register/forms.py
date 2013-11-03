65610eba184848b04c56f41b740b6b7158f348bd
Mathias Ertl <mati@fsinf.at>
2013-11-03 15:08:03 +0100

    set a help-text for registration

--- register/forms.py
+++ register/forms.py
@@ -17,7 +17,11 @@
 
 from __future__ import unicode_literals
 
+from copy import copy
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
 
 from core.forms import AntiSpamForm
 from core.forms import EmailMixin
@@ -29,8 +33,14 @@ User = get_user_model()
 
 class RegistrationForm(JidMixin, EmailMixin, AntiSpamForm):
     email = EmailMixin.EMAIL_FIELD
-    username = JidMixin.USERNAME_FIELD
+    username = copy(JidMixin.USERNAME_FIELD)
     domain = JidMixin.DOMAIN_FIELD
+
+    username.help_text = _(
+        'A minimum of %(min)s and a maximum of %(max)s characters. No "@" characters or spaces.') % {
+            'min': settings.XMPP_MIN_USERNAME_LENGTH,
+            'max': settings.XMPP_MAX_USERNAME_LENGTH,
+        }
 
 
 class RegistrationConfirmationForm(PasswordConfirmationMixin, AntiSpamForm):
