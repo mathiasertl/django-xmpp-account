01fb706774b6da7ca0fd5c7652b9f36b8dcc9971
Mathias Ertl <mati@fsinf.at>
2014-03-22 15:39:50 +0100

    actually do registrations

--- register/management/commands/ejabberd_registrations.py
+++ register/management/commands/ejabberd_registrations.py
@@ -25,11 +25,13 @@ from datetime import datetime
 import sleekxmpp
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
+from core.constants import PURPOSE_REGISTER
 from core.models import Address
 from core.models import RegistrationUser
-from core.models import UserAddress
+from core.models import UserAddresses
 
 # Python versions before 3.0 do not use UTF-8 encoding
 # by default. To ensure that Unicode is handled properly
@@ -41,34 +43,54 @@ if sys.version_info < (3, 0):
 else:
     raw_input = input
 
-REGEX = '\[([^\]]*)\]\s*([^@]*)@([^\s]*) was registered from IP address ([^\s]*)'
+User = get_user_model()
+REGEX = '\[([^\]]*)\]\s*The account ([^@]*)@([^\s]*) was registered from IP address ([^\s]*)'
+
 
 class EjabberdClient(sleekxmpp.ClientXMPP):
     """
     Example messages::
 
-        [2014-03-16 16:52:29] username@host was registered from IP address ::FFFF:1.2.3.4 on node ejabberd@host using mod_register.
-        [2014-03-16 16:52:29] username@host was registered from IP address 2001::1 on node ejabberd@host using mod_register.
+        [2014-03-16 16:52:29] The account username@host was registered from IP address ::FFFF:1.2.3.4 on node ejabberd@host using mod_register.
+        [2014-03-16 16:52:29] The account username@host was registered from IP address 2001::1 on node ejabberd@host using mod_register.
     """
 
-    def __init__(self, jid, password):
+    def __init__(self, jid, password, stdout, stderr):
         sleekxmpp.ClientXMPP.__init__(self, jid, password)
         self.add_event_handler("session_start", self.start)
         self.add_event_handler("message", self.message)
+        self.stdout = stdout
+        self.stderr = stderr
 
     def start(self, event):
-        print('start')
         self.send_presence()
         self.get_roster()
 
     def message(self, msg):
-        timestamp, user, host, ip = re.match(REGEX, msg['body'])
+        try:
+            timestamp, username, domain, ip = re.match(REGEX, msg['body']).groups()
+            timestamp = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+            address = Address.objects.get_or_create(address=ip)[0]
+
+            try:
+                user = User.objects.get(username=username, domain=domain)
+                self.stderr.write('User exists: %s@%s', (username, domain))
+                return
+            except User.DoesNotExist:
+                self.stdout.err('Creating new user: %s@%s' % (username, domain))
+                user = User.objects.create(username=username, domain=domain)
+
+            UserAddresses.objects.create(address=address, user=user, purpose=PURPOSE_REGISTER)
+        except Exception as e:
+            self.stderr.write('%s: %s' % (type(e).__name__, e))
+            return
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         bot = EjabberdClient(settings.EJABBERD_REGISTRATION_BOT['jid'],
-                             settings.EJABBERD_REGISTRATION_BOT['password'])
+                             settings.EJABBERD_REGISTRATION_BOT['password'],
+                             self.stdout, self.stderr)
 
         if bot.connect():
             bot.process(block=False)
