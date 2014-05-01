69c757261dc2ad1431e681998d237e1584d8e231
Mathias Ertl <mati@er.tl>
2014-05-01 12:33:53 +0200

    use ugettext_lazy

--- core/widgets.py
+++ core/widgets.py
@@ -18,7 +18,7 @@
 from __future__ import unicode_literals
 
 from django import forms
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 _fieldattrs = {'class': 'form-control', 'maxlength': 30, }
 _emailattrs = _fieldattrs.copy()
