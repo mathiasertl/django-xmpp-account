76f10550492e672ac856ca79d813e9c393f5ef27
Mathias Ertl <mati@er.tl>
2014-05-01 12:37:13 +0200

    remove placeholder (really useless)

--- core/widgets.py
+++ core/widgets.py
@@ -23,7 +23,6 @@ from django.utils.translation import ugettext_lazy as _
 _fieldattrs = {'class': 'form-control', 'maxlength': 30, }
 _emailattrs = _fieldattrs.copy()
 _emailattrs['type'] = 'email'
-_emailattrs['placeholder'] = _("Enter email")
 _selectattrs = _fieldattrs.copy()
 
 TextWidget = forms.TextInput(attrs=_fieldattrs)
