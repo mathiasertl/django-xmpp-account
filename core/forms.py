9b5e66eaa656e47d00166cd7df07a4b94ecf3e7e
Mathias Ertl <mati@er.tl>
2014-05-05 20:31:47 +0200

    use TextWidget for value

--- core/forms.py
+++ core/forms.py
@@ -39,7 +39,7 @@ from core.widgets import TextWidget
 
 
 class AntiSpamBase(object):
-    VALUE = forms.CharField(required=False)
+    VALUE = forms.CharField(required=False, widget=TextWidget)
     TIMESTAMP = forms.IntegerField(widget=forms.HiddenInput, required=True)
     TOKEN = forms.CharField(widget=forms.HiddenInput, required=True)
     SECURITY_HASH = forms.CharField(min_length=40, max_length=40,
