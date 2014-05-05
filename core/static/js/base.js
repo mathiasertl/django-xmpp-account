89efc3d6cae9f95bd7da31a36ee79b97caa79768
Mathias Ertl <mati@er.tl>
2014-05-05 20:55:03 +0200

    add minor jquery trick to red-label invalid JIDs

--- core/static/js/base.js
+++ core/static/js/base.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
     $('label[for="id_value"]').parent().hide();
+
+    $('#id_username').keyup(function() {
+        var input = $(this);
+        var form_group = input.parents('div.form-group');
+        if (/[@\s]/.test($(this).val())) {
+            form_group.addClass('has-error');
+        } else {
+            form_group.removeClass('has-error');
+        }
+    });
 });
