ba117ec28d716e0fdae9878ecf3c8bf26374f8fe
Mathias Ertl <mati@er.tl>
2014-11-09 19:51:07 +0100

    add two-click like to (every...) page

--- core/static/js/base.js
+++ core/static/js/base.js
@@ -23,4 +23,21 @@ $(document).ready(function() {
             form_group.addClass('has-error');
         }
     });
+
+    $('.fblike').on('click', function(event) {
+        // Generate a string containing the HTML to place in the element (for readability)
+        var html = "<div id=\"fb-root\">\n";
+        html += "<div class=\"fb-like\" data-href=\"https://facebook.com/jabber.at\" data-send=\"true\" data-layout=\"button_count\" data-width=\"100\" data-show-faces=\"false\" data-font=\"arial\">\n";
+        html += "</div>\n";
+
+        // Replace the specified element's contents with the HTML necessary to display the
+        // Like/+1 Buttons, *before* loading the SDKs below
+        $('.fblike').html(html);
+
+        // This is the code provided by facebook to asynchronously load their SDK
+        var e = document.createElement('script'); e.async = true;
+        e.src = document.location.protocol +
+          '//connect.facebook.net/en_US/all.js#xfbml=1';
+        document.getElementById('fb-root').appendChild(e);
+    });
 });
