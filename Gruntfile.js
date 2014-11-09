e0becb47022e19c04e0afa91def464cfd9e23263
Mathias Ertl <mati@er.tl>
2014-11-09 19:24:20 +0100

    remove concat task

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,7 +36,6 @@ module.exports = function(grunt) {
 
   // Load tasks
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
