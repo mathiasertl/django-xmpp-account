fc94dcb38bb7b4fae1d0c8545b24b823b1503d75
Mathias Ertl <mati@er.tl>
2014-11-11 22:38:17 +0100

    tw=99

--- backends/base.py
+++ backends/base.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of xmppregister (https://account.jabber.at/doc).
+# This file is part of django-xmpp-register (https://account.jabber.at/doc).
 #
-# xmppregister is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# django-xmpp-register is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
 #
-# xmppregister is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# django-xmpp-register is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+# the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with xmppregister.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with
+# django-xmpp-register.  If not, see <http://www.gnu.org/licenses/>.
 
 """Common code for XMPP backends."""
 
@@ -50,10 +48,9 @@ class XmppBackendBase(object):
     library = None
     """Import-party of any third-party library you need.
 
-    Set this attribute to an import path and you will be able to access the
-    module as ``self.module``. This way you don't have to do a module-level
-    import, which would mean that everyone has to have that library installed,
-    even if they're not using your backend.
+    Set this attribute to an import path and you will be able to access the module as
+    ``self.module``. This way you don't have to do a module-level import, which would mean that
+    everyone has to have that library installed, even if they're not using your backend.
     """
     _module = None
 
@@ -64,8 +61,7 @@ class XmppBackendBase(object):
         if self._module is None:
             if self.library is None:
                 raise ValueError(
-                    "Backend '%s' doesn't specify a library attribute" %
-                    self.__class__)
+                    "Backend '%s' doesn't specify a library attribute" % self.__class__)
 
             if isinstance(self.library, (tuple, list)):
                 name, mod_path = self.library
@@ -74,8 +70,7 @@ class XmppBackendBase(object):
             try:
                 module = importlib.import_module(mod_path)
             except ImportError:
-                raise ValueError("Couldn't load %s backend library library"
-                                 % name)
+                raise ValueError("Couldn't load %s backend library library" % name)
             self._module = module
         return self._module
 
@@ -89,24 +84,20 @@ class XmppBackendBase(object):
     def reserve(self, username, domain, email):
         """Reserve a new account.
 
-        This method is called when the user first registers for an account on a
-        domain with its ``RESERVE`` setting set to True (see
-        :ref:`settings-XMPP_HOSTS`). The email address is not yet confirmed and
-        the user hasn't provided a password yet, so login via XMPP should be
+        This method is called when the user first registers for an account on a domain with its
+        ``RESERVE`` setting set to True (see :ref:`settings-XMPP_HOSTS`). The email address is not
+        yet confirmed and the user hasn't provided a password yet, so login via XMPP should be
         impossible at this point.
 
-        The default implementation calls
-        :py:func:`~backends.base.XmppBackendBase.create` with password=None.
-        You can override this if your account supports a true "reservation"
-        method that blocks registration via other means but does not allow the
-        user to log in.
+        The default implementation calls :py:func:`~backends.base.XmppBackendBase.create` with
+        password=None.  You can override this if your account supports a true "reservation" method
+        that blocks registration via other means but does not allow the user to log in.
 
         :param username: The username of the new user.
         :param   domain: The selected domain, may be any domain provided
                          in :ref:`settings-XMPP_HOSTS`.
-        :param    email: The email address provided by the user. Note that at
-                         this point it is not confirmed. You are free to ignore
-                         this parameter.
+        :param    email: The email address provided by the user. Note that at this point it is not
+                         confirmed. You are free to ignore this parameter.
         """
         self.create(username=username, domain=domain, password=None,
                     email=email)
@@ -114,15 +105,13 @@ class XmppBackendBase(object):
     def create(self, username, domain, password, email):
         """Create a new user.
 
-        If the domains ``RESERVE`` setting is ``True`` (see
-        :ref:`settings-XMPP_BACKENDS`) and your backend does not override
-        :py:func:`~backends.base.XmppBackendBase.reserve`, then the password is
-        ``None`` if this function is called via
-        :py:func:`~backends.base.XmppBackendBase.reserve`, in which case you can
-        use :py:func:`~backends.base.XmppBackendBase.get_random_password` for a
-        password. If the password is not ``None``, you have to consider that
-        the user already exists (with a random password) in the backend and you
-        only have to set his/her email and password::
+        If the domains ``RESERVE`` setting is ``True`` (see :ref:`settings-XMPP_BACKENDS`) and your
+        backend does not override :py:func:`~backends.base.XmppBackendBase.reserve`, then the
+        password is ``None`` if this function is called via
+        :py:func:`~backends.base.XmppBackendBase.reserve`, in which case you can use
+        :py:func:`~backends.base.XmppBackendBase.get_random_password` for a password. If the
+        password is not ``None``, you have to consider that the user already exists (with a random
+        password) in the backend and you only have to set his/her email and password::
 
             from django.conf import settings
             from backends.base import XmppBackendBase
@@ -145,9 +134,8 @@ class XmppBackendBase(object):
         :param      domain: The selected domain, may be any domain provided
                             in :ref:`settings-XMPP_HOSTS`.
         :param    password: The password of the new user.
-        :param       email: The email address provided by the user. Note that at
-                            this point it is not confirmed. You are free to ignore
-                            this parameter.
+        :param       email: The email address provided by the user. Note that at this point it is
+                            not confirmed. You are free to ignore this parameter.
         """
         raise NotImplementedError
 
@@ -175,8 +163,7 @@ class XmppBackendBase(object):
     def set_unusable_password(self, username, domain):
         """Called when a user is blocked.
 
-        The default implementation calls :py:func:`set_password` with a random
-        password.
+        The default implementation calls :py:func:`set_password` with a random password.
         """
         self.set_password(username, domain, self.get_random_password())
 
@@ -194,10 +181,9 @@ class XmppBackendBase(object):
     def expire(self, username, domain):
         """Expire a username reservation.
 
-        This method is called when the confirmation key for a registration
-        expires. The default implementation just calls
-        :py:func:`~backends.base.XmppBackendBase.remove`. This is fine if you
-        do not override :py:func:`~backends.base.XmppBackendBase.reserve`.
+        This method is called when the confirmation key for a registration expires. The default
+        implementation just calls :py:func:`~backends.base.XmppBackendBase.remove`. This is fine if
+        you do not override :py:func:`~backends.base.XmppBackendBase.reserve`.
         """
         self.remove(username, domain)
 
@@ -208,8 +194,7 @@ class XmppBackendBase(object):
     def remove(self, username, domain):
         """Remove a user.
 
-        This method is called when the user explicitly wants to remove her/his
-        account.
+        This method is called when the user explicitly wants to remove her/his account.
 
         :param username: The username of the new user.
         :param   domain: The domainname selected, may be any domainname
