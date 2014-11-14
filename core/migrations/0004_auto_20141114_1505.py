d74284422dd085934ed5b5aa9f4b4ec60275e42c
Mathias Ertl <mati@er.tl>
2014-11-14 15:09:34 +0100

    add data migration to lowercase all usernames in the database

---
+++ core/migrations/0004_auto_20141114_1505.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+def lowercase(apps, schema_editor):
+    RegistrationUser = apps.get_model('core', 'RegistrationUser')
+    for user in RegistrationUser.objects.all():
+        lowercased = user.username.lower()
+        if lowercased != user.username:
+            user.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0003_registrationuser_registration_method'),
+    ]
+
+    operations = [
+        migrations.RunPython(lowercase),
+    ]
