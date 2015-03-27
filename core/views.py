a12f14354fb2e4ff3ff792de07d9a339d034face
Mathias Ertl <mati@er.tl>
2015-03-27 23:16:34 +0100

    catch keyError again

--- core/views.py
+++ core/views.py
@@ -84,7 +84,6 @@ class AntiSpamFormView(FormView):
         except RecaptchaUnreachableError:
             raise TemporaryError(_("The ReCAPTCHA server was unreacheable."))
         except KeyError:
-            raise
             raise TemporaryError(_("The ReCAPTCHA didn't work properly."))
 
     def get_context_data(self, **kwargs):
