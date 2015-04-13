bf6002c4faa1cd739d158ae6130e6c66838cf309
Mathias Ertl <mati@er.tl>
2015-04-13 20:04:30 +0200

    enforce max lenght of 255

---
+++ core/migrations/0010_auto_20150413_2004.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0009_auto_20150410_1820'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='registrationuser',
+            name='jid',
+            field=models.CharField(unique=True, max_length=255, verbose_name='JID'),
+        ),
+    ]
