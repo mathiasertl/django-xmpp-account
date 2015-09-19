3b9efda885a534f092fe4efd52d508e6f086cf01
Mathias Ertl <mati@er.tl>
2015-09-19 11:57:11 +0200

    cleanup

--- core/static/js/base.js
+++ core/static/js/base.js
@@ -148,14 +148,10 @@ $(document).ready(function() {
     });
 
     $('.js-captcha-refresh').click(function(){
-        console.log('refresh clicked!');
         $form = $(this).parents('form');
-        console.log(captcha_refresh_url);
 
         $.getJSON(captcha_refresh_url, {}, function(json) {
             // This should update your captcha image src and captcha hidden input
-            console.log(json);
-            console.log(typeof json);
             $('#captcha-form-data img').attr('src', json.image_url);
             $('#captcha-form-data input[type="hidden"]').attr('value', json.key);
         });
