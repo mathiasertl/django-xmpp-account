d9df0313973aa0e90e6347e4df29a9c12bec0547
Mathias Ertl <mati@er.tl>
2016-06-12 20:48:13 +0200

    add some constants

---
+++ xmpp_accounts/constants.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# vim: expandtab:tabstop=4:hlsearch
+#
+# This file is part of django-xmpp-account (https://github.com/mathiasertl/django-xmpp-account/).
+#
+# django-xmpp-account is free software: you can redistribute it and/or modify it under the terms of
+# the GNU General Public License as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# django-xmpp-account is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+# the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-xmpp-account.
+# If not, see <http://www.gnu.org/licenses/>.
+
+PURPOSE_REGISTER = 'register'
+PURPOSE_SET_EMAIL = 'email'
+PURPOSE_SET_PASSWORD = 'password'
+PURPOSE_DELETE = 'delete'
