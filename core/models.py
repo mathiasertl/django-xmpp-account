60f171530a5c2bea656c9e4e28aea0b9ea0e49a8
Mathias Ertl <mati@er.tl>
2016-09-02 12:30:04 +0200

    finixh migration of purpose

--- core/models.py
+++ core/models.py
@@ -376,8 +376,7 @@ class UserAddresses(models.Model):
     user = models.ForeignKey(RegistrationUser)
 
     timestamp = models.DateTimeField(auto_now_add=True)
-    purpose = models.SmallIntegerField(choices=PURPOSE_CHOICES)
-    new_purpose = models.CharField(max_length=12, choices=NEW_PURPOSE_CHOICES, null=True)
+    purpose = models.CharField(max_length=12, choices=NEW_PURPOSE_CHOICES)
 
     class Meta:
         verbose_name = _('IP-Address Activity')
